2bed19aab4efc5876da7230f15438b8728ee7dd8
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-03-11 13:47:04 +0000

    Third Commit: Reading Input Data

--- app.js
+++ app.js
@@ -10,7 +10,25 @@ var budgetController = (function(){
 
 var UIController = (function() {
 
+var DOMstrings = {
+  inputType: '.add__type',
+  inputDescription: '.add__description',
+  inputValue: '.add__value',
+  inputBtn: '.add__btn'
+}
 
+  return{
+    getinput: function(){
+      return{
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function(){
+      return DOMstrings;
+    }
+  };
 
 })();
 
@@ -18,11 +36,26 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function(){
-    console.log('It works!');
+    //1. Get the field input data
+
+    var input =  UICtrl.getinput();
+    console.log(input);
+
+    //2. Add the item to the budget controller
+
+    //3. Add the item to the UI
+
+    //4. Calculate the budget
+
+    //5. Display the budget on the UI
+
+
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event){
     if(event.keyCode === 13 || event.which === 13){
