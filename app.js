3a75b8a266849a3b61262ddc5658a66049a84fab
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-03-19 19:10:02 +0000

    Eleventh Commit: Updating the Budget, Income and Expenses values.

--- app.js
+++ app.js
@@ -99,6 +99,10 @@ var DOMstrings = {
   inputBtn: '.add__btn',
   incomeContainer: '.income__list',
   expensesContainer: '.expenses__list',
+  budgetLabel: '.budget__value',
+  incomeLabel: '.budget__income--value',
+  expensesLabel: '.budget__expenses--value',
+  percentageLabel: '.budget__expenses--percentage',
 }
 
   return{
@@ -115,10 +119,10 @@ var DOMstrings = {
       // Create HTML string with placeholder text
         if (type === 'inc') {
           element = DOMstrings.incomeContainer;
-          html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value"> %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
         } else if (type === 'exp'){
           element = DOMstrings.expensesContainer;
-          html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value"> %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
         }
 
       // Replace the placeholder text with some actual data
@@ -139,6 +143,17 @@ var DOMstrings = {
        });
        fieldsArr[0].focus();
     },
+
+    displayBudget: function(obj) {
+        document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+        document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+        document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            if (obj.percentage > 0) {
+              document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+              document.querySelector(DOMstrings.percentageLabel).textContent = '--'
+            }
+      },
 
     getDOMstrings: function(){
       return DOMstrings;
@@ -171,7 +186,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var budget = budgetCtrl.getBudget();
 
     //3. Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   var ctrlAddItem = function(){
@@ -200,6 +215,12 @@ var controller = (function(budgetCtrl, UICtrl) {
   return{
     init: function(){
       console.log('Application has started!!!');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
