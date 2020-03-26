df5aad878fc7cea6b9935ca7c6fb28fc5c0eafac
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-03-26 13:18:58 +0000

    Thirteenth Commit: Deleting an Item from the Budget (Budget Controller)

--- app.js
+++ app.js
@@ -56,6 +56,24 @@ var budgetController = (function(){
         // Return the new element
         return newItem;
     },
+    deleteItem: function(type, id){
+      var ids, index;
+      // id = 6
+      // data.allItems[type][id];
+      // ids = [1 2 4 6 8]
+      // index = 3
+
+      ids = data.allItems[type].map(function(current){
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if(index !== -1){
+          data.allItems[type].splice(index, 1);
+      };
+
+    },
 
     calculateBudget: function(){
         //1. Calculate total income and expenses
@@ -226,10 +244,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         //inc-1
         splitID = itemID.split('-');
         type = splitID[0];
-        ID = splitID[1];
+        ID = parseInt(splitID[1]);
 
         //1. Delete the item from the data structure
-
+        budgetCtrl.deleteItem(type, ID);
         //2. Delete the item from the UI
 
         //3. Update and show the new budget
