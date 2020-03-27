dfb7d5e962961660e4550ab1e17135eb6ce9a8f9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-03-27 16:49:13 +0000

    Twentieth Commit: Updated UI input field edges to toggle red/blue colour

--- app.js
+++ app.js
@@ -168,6 +168,12 @@ var DOMstrings = {
     return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
   };
 
+  var nodeListForEach = function(list, callback){
+      for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+      }
+  };
+
   return{
     getinput: function(){
       return{
@@ -228,12 +234,6 @@ var DOMstrings = {
       displayPercentages: function(percentages){
           var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-          var nodeListForEach = function(list, callback){
-              for (var i = 0; i < list.length; i++) {
-                  callback(list[i], i);
-              }
-          };
-
           nodeListForEach(fields, function(current, index){
             if(percentages[index] > 0){
                 current.textContent = percentages[index] + '%';
@@ -249,12 +249,26 @@ var DOMstrings = {
           months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
           month = now.getMonth();
           year = now.getFullYear();
-          document.querySelector(DOMstrings.dateLabel).textContent = months[month] + '-' + year;
+          document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' - ' + year;
         },
 
-    getDOMstrings: function(){
-      return DOMstrings;
-    }
+        changedType: function(){
+
+          var fields = document.querySelectorAll(
+              DOMstrings.inputType + ',' +
+              DOMstrings.inputDescription + ',' +
+              DOMstrings.inputValue);
+
+          nodeListForEach(fields, function(cur){
+              cur.classList.toggle('red-focus');
+
+          document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+          });
+        },
+
+        getDOMstrings: function(){
+          return DOMstrings;
+        }
 
   };
 
@@ -274,6 +288,8 @@ var controller = (function(budgetCtrl, UICtrl) {
       });
 
       document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+      document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
     };
 
