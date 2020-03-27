ca429c112240a5579448d9321bb431496f0e5b82
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-03-27 13:24:50 +0000

    Seventeenth Commit: Updating Percentages - UI Controller

--- app.js
+++ app.js
@@ -148,6 +148,7 @@ var DOMstrings = {
   expensesLabel: '.budget__expenses--value',
   percentageLabel: '.budget__expenses--percentage',
   container: '.container',
+  expensesPercLabel: '.item__percentage',
 }
   return{
     getinput: function(){
@@ -200,8 +201,26 @@ var DOMstrings = {
             if (obj.percentage > 0) {
               document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
             } else {
-              document.querySelector(DOMstrings.percentageLabel).textContent = '--'
+              document.querySelector(DOMstrings.percentageLabel).textContent = '---'
             }
+      },
+
+      displayPercentages: function(percentages){
+          var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+          var nodeListForEach = function(list, callback){
+              for (var i = 0; i < list.length; i++) {
+                  callback(list[i], i);
+              }
+          };
+
+          nodeListForEach(fields, function(current, index){
+            if(percentages[index] > 0){
+                current.textContent = percentages[index] + '%';
+            }else {
+                current.textContent = '---';
+            }
+          });
       },
 
     getDOMstrings: function(){
@@ -250,7 +269,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var percentages = budgetCtrl.getPercentages();
 
     //3. Update the UI with the new percentages
-    console.log(percentages);
+    UICtrl.displayPercentages(percentages);
 
   };
 
